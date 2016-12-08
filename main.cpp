eb586b3c204d88297ce9d9c168c2f1a04848f5ee
guillaume lesieur <guillaumelesieur4@gmail.com>
2016-12-09 00:20:49 +0100

    dd

--- main.cpp
+++ main.cpp
@@ -43,16 +43,16 @@ bool readFile(City tab[]){
     double lon, lat;
     bool firstLine = true;
     int i = 0;
-
+    
     cout << "Reading the file..." << endl;
-    ifstream file("/Users/guillaumelesieur/Downloads/city.txt", ios::in); // a changer chez vous pour tester
+    ifstream file("/Users/guillaumelesieur/Downloads/city.txt", ios::in); // corresponding to my computer
     if (file.is_open())
     {
         
         while (getline(file, fullString))
         {
             if (!firstLine){
-                cout << "New line : " << fullString << endl;
+                //cout << "New line : " << fullString << endl;
                 
                 string s = fullString;
                 string delimiter = ",";
@@ -94,7 +94,7 @@ bool readFile(City tab[]){
                     return false;
                 }
                 
-                cout << std::setprecision(17) << "New data : " << name << " | " << lat << " | " << lon << endl;
+                cout << std::setprecision(17) << name << " | " << lat << " | " << lon << endl;
                 cout << endl;
                 
                 City city;
@@ -110,9 +110,6 @@ bool readFile(City tab[]){
         }
         file.close();    }
     else cout << "Unable to open file";
-    
-    
-    
     
     return true;
     
@@ -143,32 +140,80 @@ void convert_in_graph(City tab[]){
     int numberCities=71;
     long double matrix[n][n];
     int dist;
-
+    
     // Fill the distance with INFINITY
     for(int i=0; i<numberCities; i++){
         matrix[i][j] = INFINITY;
     }
     
     for(int i=0; i<numberCities; i++){
-      for (j=0; j<numberCities; j++) {
-          city1 = tab[i];
-          city2 = tab[j];
-          dist = calculDistance(city1.lat, city1.lon, city2.lat, city2.lon);
+        for (j=0; j<numberCities; j++) {
+            city1 = tab[i];
+            city2 = tab[j];
+            dist = calculDistance(city1.lat, city1.lon, city2.lat, city2.lon);
             
-          if (i==j){
-              dist= 0;
-          }
-          cout << city1.name << " to " << city2.name << " : " << dist << endl;
-          //if (dist>100) matrix[i][j]=dist;
-      }
+            if (i==j){
+                dist= 0;
+            }
+            cout << city1.name << " to " << city2.name << " : " << dist << endl;
+            //if (dist>100) matrix[i][j]=dist;
+        }
     }
     
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Lin-Kernighan heuristic
+// 2 opt
 
+//int nearest_neighbor()
+//{
+//    solution.clear();
+//    int total_dist = 0;
+//    int best_start_distance = 9999999;
+//    int last_run = 0;
+//    
+//    //Run through entire 2-opt optimization for each city, write best index.
+//    for (int i = 0; !done && i < num_cities; ++i)
+//    {
+//        nearest_neighbor_basic(i);
+//        last_run = two_change();
+//        if (last_run < best_start_distance)
+//        {
+//            best_start_distance = last_run;
+//            cout << "Writing solution " << best_start_distance << endl;
+//            write_solution(OUTPUT_FN);  //write each time an improvement is found
+//        }
+//    }
+//    
+//    total_dist = get_solution_distance();
+//    if (best_start_distance <= total_dist)
+//        return best_start_distance;     //solution already written
+//    
+//    else
+//    {
+//        cout << "Writing solution " << total_dist << endl;
+//        write_solution(OUTPUT_FN);       //write current solution (midway through 2-opt)
+//        return best_start_distance;
+//    }
+//}
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 int main(int argc, const char * argv[]) {
@@ -179,7 +224,7 @@ int main(int argc, const char * argv[]) {
             string ligne;
             cout << "Lecture de fichier : "<< endl;
             //while(getline(fichier, ligne)){
-                //cout << ligne << endl;  // on l'affiche
+            //cout << ligne << endl;  // on l'affiche
             //}
         }
         City cities[71];

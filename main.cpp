fea0fe01bf26f0d72147651203a265e212cf6b26
Maxence Pinard <maxence.pinard@devinci.fr>
2016-12-09 09:15:06 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -17,8 +17,7 @@
 #include <stdlib.h>
 #include <iomanip>
 #include <cmath>
-#include "City.hpp"
-
+#include "City.h"
 
 #ifdef NAN
 #endif
@@ -43,32 +42,32 @@ bool readFile(City tab[]){
     double lon, lat;
     bool firstLine = true;
     int i = 0;
-    
+
     cout << "Reading the file..." << endl;
-    ifstream file("/Users/guillaumelesieur/Downloads/city.txt", ios::in); // corresponding to my computer
+    ifstream file("Cites.csv", ios::in); // a changer chez vous pour tester
     if (file.is_open())
     {
-        
+
         while (getline(file, fullString))
         {
             if (!firstLine){
-                //cout << "New line : " << fullString << endl;
-                
+                cout << "New line : " << fullString << endl;
+
                 string s = fullString;
                 string delimiter = ",";
-                
+
                 size_t pos = 0;
                 string token;
-                
+
                 // Get the name
                 pos = s.find(delimiter);
                 name = s.substr(0, pos);
                 s.erase(0, pos + delimiter.length());
-                
+
                 //Get the latitude
                 pos = s.find(delimiter);
                 val2 = s.substr(0, pos);
-                
+
                 stringstream myStream2(val2);// This code converts from string to number safely.
                 if (myStream2 >> lat){
                     lat = atof(val2.c_str());
@@ -78,11 +77,11 @@ bool readFile(City tab[]){
                     cout << "ERROR, INVALID DATA !" << endl;
                     return 1;
                 }
-                
+
                 //Get the longitude
                 pos = s.find(delimiter);
                 val1 = s.substr(0, pos);
-                
+
                 stringstream myStream(val1);// This code converts from string to number safely.
                 if (myStream >> lon)
                 {
@@ -93,26 +92,29 @@ bool readFile(City tab[]){
                     cout << "ERROR, INVALID DATA !" << endl;
                     return false;
                 }
-                
-                cout << std::setprecision(17) << name << " | " << lat << " | " << lon << endl;
+
+                cout << std::setprecision(17) << "New data : " << name << " | " << lat << " | " << lon << endl;
                 cout << endl;
-                
+
                 City city;
                 city.lat = lat;
                 city.lon = lon;
                 city.name = name;
                 tab[i] = city;
-                
+
                 i++;
-                
+
             }
             firstLine = false;
         }
         file.close();    }
     else cout << "Unable to open file";
-    
+
+
+
+
     return true;
-    
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -135,96 +137,55 @@ long double calculDistance(long double lat1, long double lon1, long double lat2,
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void convert_in_graph(City tab[]){
-    int i,j,n;
+    int i,j;
     City city1, city2;
     int numberCities=71;
-    long double matrix[n][n];
+    long double matrix[numberCities][numberCities];
     int dist;
-    
+
     // Fill the distance with INFINITY
-    for(int i=0; i<numberCities; i++){
-        matrix[i][j] = INFINITY;
-    }
-    
-    for(int i=0; i<numberCities; i++){
-        for (j=0; j<numberCities; j++) {
-            city1 = tab[i];
-            city2 = tab[j];
-            dist = calculDistance(city1.lat, city1.lon, city2.lat, city2.lon);
-            
-            if (i==j){
-                dist= 0;
-            }
-            cout << city1.name << " to " << city2.name << " : " << dist << endl;
-            //if (dist>100) matrix[i][j]=dist;
+    for(i=0; i<numberCities; i++){
+        for (j=0; j<numberCities; j++)
+        {
+            matrix[i][j] = INFINITY;
         }
     }
-    
+
+    for(i=0; i<numberCities; i++){
+      for (j=0; j<numberCities; j++) {
+          city1 = tab[i];
+          city2 = tab[j];
+          dist = calculDistance(city1.lat, city1.lon, city2.lat, city2.lon);
+
+          if (i==j){
+              dist= 0;
+          }
+          cout << city1.name << " to " << city2.name << " : " << dist << endl;
+          //if (dist>100) matrix[i][j]=dist;
+      }
+    }
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Lin-Kernighan heuristic
-// 2 opt
+//Choisir une ville A de départ et la suivante S(A)
+//Choisir une ville B et la suivante S(B)
+//Si le coût pour aller de A à B + S(A) à S(B) est inférieur au coût de A à S(A) + B à S(B), échanger ces solutions (faire ça pour tous les chemins possibles)
 
-//int nearest_neighbor()
-//{
-//    solution.clear();
-//    int total_dist = 0;
-//    int best_start_distance = 9999999;
-//    int last_run = 0;
-//    
-//    //Run through entire 2-opt optimization for each city, write best index.
-//    for (int i = 0; !done && i < num_cities; ++i)
-//    {
-//        nearest_neighbor_basic(i);
-//        last_run = two_change();
-//        if (last_run < best_start_distance)
-//        {
-//            best_start_distance = last_run;
-//            cout << "Writing solution " << best_start_distance << endl;
-//            write_solution(OUTPUT_FN);  //write each time an improvement is found
-//        }
-//    }
-//    
-//    total_dist = get_solution_distance();
-//    if (best_start_distance <= total_dist)
-//        return best_start_distance;     //solution already written
-//    
-//    else
-//    {
-//        cout << "Writing solution " << total_dist << endl;
-//        write_solution(OUTPUT_FN);       //write current solution (midway through 2-opt)
-//        return best_start_distance;
-//    }
-//}
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 int main(int argc, const char * argv[]) {
     //on ouvre le fichier en lecture
-    ifstream fichier("/Users/guillaumelesieur/Downloads/city.txt", ios::in);
+    ifstream fichier("Cites.csv", ios::in);
     if(fichier){
         if (fichier){
             string ligne;
             cout << "Lecture de fichier : "<< endl;
             //while(getline(fichier, ligne)){
-            //cout << ligne << endl;  // on l'affiche
+                //cout << ligne << endl;  // on l'affiche
             //}
         }
         City cities[71];
@@ -234,7 +195,7 @@ int main(int argc, const char * argv[]) {
     }
     else
         cout << "Impossible d'ouvrir le fichier !" << endl;
-    
+
     return 0;
 }
 
